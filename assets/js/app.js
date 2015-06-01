67619c23e0df91512c48957cef293e682ed97cf9
Kemal Fadillah <fadillzzz@gmail.com>
2015-06-01 10:04:50 +0700

    Restricts routes based on authentication

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,18 @@ TCP.config(['$routeProvider', function ($routeProvider)
 {
   $routeProvider.when('/sign-in', {
     templateUrl: 'templates/sign-in.html',
+    redirectTo: function () {
+      if (window.user) {
+        return '/profile';
+      }
+    }
   }).when('/profile', {
     templateUrl: 'templates/profile.html',
+    redirectTo: function () {
+      if ( ! window.user) {
+        return '/sign-in';
+      }
+    }
   }).otherwise({
     redirectTo: window.user ? '/profile' : '/sign-in'
   });
